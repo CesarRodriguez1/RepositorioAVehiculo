14972e31a9debe23d77323d05227f1c2c040d612
cesar daniel rodriguez castro <cesar_love1996@hotmail.com>
2017-07-23 14:44:48 -0500

    Proyecto Primera Parte

---
+++ AlquilerVehiculo_DA/AlquilerVehiculo/Areas/Administrador/Controllers/MainController.cs
@@ -0,0 +1,18 @@
+﻿using AlquilerVehiculo_DA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlquilerVehiculo.Areas.Administrador.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Administrador/Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AlquilerVehiculo_DA/AlquilerVehiculo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlquilerVehiculo.Startup))]
+namespace AlquilerVehiculo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
